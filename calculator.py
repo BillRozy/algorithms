f66404dbed4ef75ea50884a1a52553ad6af05a07
Oleg Budylin <Oleg.Budylin@harman.com>
2018-09-28 12:41:34 +0300

    working calc

--- calculator.py
+++ calculator.py
@@ -1,27 +1,41 @@
 import numpy as np
 from collections import deque
 
-def calculate(end, start=1, memoize=None):
-    memoize = memoize or [float('inf') for i in range(0, end + 1)]
+
+def calculate(end):
+    memoize = [float('inf') for i in range(0, end)]
     memoize[0] = 0
-    memoize[1] = 0
-    print(end, start)
-    if start == end:
-        print(memoize)
-        return memoize[end]
-    doubled = start * 2
-    tripled = start * 3
-    inced = start + 1
-    if doubled <= end:
-        memoize[doubled] = min(memoize[doubled], memoize[start] + 1)
-        calculate(end, doubled, memoize)
-    if tripled <= end:
-        memoize[tripled] = min(memoize[tripled], memoize[start] + 1)
-        calculate(end, tripled, memoize)
-    if inced <= end:
-        memoize[inced] = min(memoize[inced], memoize[start] + 1)
-        calculate(end, inced, memoize)
-    return memoize[end]
+    trap = set()
+    turn_numbers = set()
+    turn_numbers.add(1)
+    for i in range(1, end + 1):
+        print('i', i)
+        if memoize[end - 1] != float('inf'):
+            print(trap)
+            return memoize[end - 1]
+        this_turn_numbers = set()
+        for num in list(turn_numbers):
+            # if memoize[num - 1] != float('inf'):
+            #     continue
+            doubled = num * 2
+            tripled = num * 3
+            inced = num + 1
+            if doubled <= end and memoize[doubled - 1] == float('inf'):
+                memoize[doubled - 1] = i
+                this_turn_numbers.add(doubled)
+            if tripled <= end and memoize[tripled - 1] == float('inf'):
+                memoize[tripled - 1] = i
+                this_turn_numbers.add(tripled)
+            if inced <= end and memoize[inced - 1] == float('inf'):
+                memoize[inced - 1] = i
+                this_turn_numbers.add(inced)
+        print('new numbers', this_turn_numbers)
+        trap.add(max(list(this_turn_numbers)))
+        turn_numbers = this_turn_numbers
+        this_turn_numbers = set()
+    print(memoize)
+    print(trap)
+    return memoize[end - 1]
 
 def test():
     ops = calculate(1)
